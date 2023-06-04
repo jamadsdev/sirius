870ac90daed6b4666a4d3930e0e0b1773b7db2fd
Adam Thompson <arthompson@gmail.com>
2023-06-04 09:30:13 -0400

    Update to user display

--- src/routes/(wxc)/wxc/vmpinreset/+page.server.js
+++ src/routes/(wxc)/wxc/vmpinreset/+page.server.js
@@ -1,17 +1,18 @@
 /** @type {import('./$types').PageServerLoad} */
-import * as api from '$lib/wxc_api';
+import * as api from "$lib/wxc_api";
 // import { fail } from '@sveltejs/kit';
 
 export const load = async ({ locals }) => {
-	const fetchUsers = async () => {
-		const { items } = await api.get('people?callingData=true', locals.user.access);
-		console.log('Users', items);
-		return items;
-	};
+  const fetchUsers = async () => {
+    const { items } = await api.get(
+      "people?callingData=true",
+      locals.user.access
+    );
+    console.log(items);
+    return items;
+  };
 
-	return {
-		streamed: {
-			users: fetchUsers()
-		}
-	};
+  return {
+    streamed: { users: fetchUsers() },
+  };
 };
