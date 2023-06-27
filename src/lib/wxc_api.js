76579b6f37113bb24a7f05939247a514ef07629e
Adam Thompson <arthompson@gmail.com>
2023-06-27 12:29:42 -0400

    vmpin feature start

--- src/lib/wxc_api.js
+++ src/lib/wxc_api.js
@@ -1,40 +1,41 @@
-import { error } from '@sveltejs/kit';
+import { error } from "@sveltejs/kit";
 
-const base = 'https://webexapis.com/v1';
+const base = "https://webexapis.com/v1";
 
 async function send({ method, path, data, token }) {
-	const opts = { method, headers: {} };
+  const opts = { method, headers: {} };
 
-	if (data) {
-		opts.headers['Content-Type'] = 'application/json';
-		opts.body = JSON.stringify(data);
-	}
+  if (data) {
+    opts.headers["Content-Type"] = "application/json";
+    opts.body = JSON.stringify(data);
+  }
 
-	if (token) {
-		opts.headers['Authorization'] = `Bearer ${token}`;
-	}
+  if (token) {
+    opts.headers["Authorization"] = `Bearer ${token}`;
+  }
 
-	const res = await fetch(`${base}/${path}`, opts);
-	if (res.ok || res.status == 422) {
-		const text = await res.text();
-		return text ? JSON.parse(text) : {};
-	}
+  const res = await fetch(`${base}/${path}`, opts);
+  if (res.ok || res.status == 422) {
+    const text = await res.text();
 
-	throw error(res.status);
+    return text ? JSON.parse(text) : {};
+  }
+
+  throw error(res.status);
 }
 
 export function get(path, token) {
-	return send({ method: 'GET', path, token });
+  return send({ method: "GET", path, token });
 }
 
 export function del(path, token) {
-	return send({ method: 'DELETE', path, token });
+  return send({ method: "DELETE", path, token });
 }
 
 export function post(path, data, token) {
-	return send({ method: 'POST', path, data, token });
+  return send({ method: "POST", path, data, token });
 }
 
 export function put(path, data, token) {
-	return send({ method: 'PUT', path, data, token });
+  return send({ method: "PUT", path, data, token });
 }
